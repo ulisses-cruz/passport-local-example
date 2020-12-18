8f547757a7b290668f7e50bb7d1b2be4a257468f
Ulisses Cruz <ulissescruz10@gmail.com>
2020-12-18 08:50:23 -0100

    auth controller updated to fix bug

--- config/passport.config.js
+++ config/passport.config.js
@@ -1,6 +1,6 @@
-const passport = require("passport");
-const localStrategy = require("passport-local").Strategy;
-const bcrypt = require("bcrypt");
+const passport = require("passport")
+const localStrategy = require("passport-local").Strategy
+const bcrypt = require("bcrypt")
 
 // you should use a database instead
 // this is just for exemplification
@@ -10,27 +10,27 @@ const db = [
     email: "email@example.com",
     password: bcrypt.hashSync("secret", 10),
   },
-];
+]
 
 passport.use(
   new localStrategy({ usernameField: "email" }, (email, password, done) => {
     // check if email exists
-    const user = db.find((u) => u.email === email);
-    if (!user) return done(null, false);
+    const user = db.find((u) => u.email === email)
+    if (!user) return done(null, false)
 
     // if email exists check password
     if (!bcrypt.compareSync(password, user.password)) {
-      return done(null, false);
+      return done(null, false)
     }
 
-    done(null, user);
+    done(null, user)
   })
-);
+)
 
-passport.serializeUser((user, done) => done(null, user.id));
+passport.serializeUser((user, done) => done(null, user.id))
 passport.deserializeUser((id, done) => {
-  const user = db.find((u) => u.id == id);
-  done(null, user);
-});
+  const user = db.find((u) => u.id == id)
+  done(null, user)
+})
 
-module.exports = passport;
+module.exports = passport
